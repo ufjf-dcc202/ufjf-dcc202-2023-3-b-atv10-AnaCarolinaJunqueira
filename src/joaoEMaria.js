b5dd07ce9b3a19f2f32bcbdba25ecbe37600a63f
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2023-12-05 11:12:37 -0300

    criando as funções

--- src/joaoEMaria.js
+++ src/joaoEMaria.js
@@ -1,2 +1,29 @@
 let joao = 1;
 let maria = 2;
+
+function deJoaoParaMaria(){
+    maria += joao;
+    joao = 0;
+}
+
+function deMariaParaJoao(){
+    joao += maria;
+    maria = 0;
+
+}
+
+function getJoao(){
+    return joao;
+}
+
+function getMaria(){
+    return maria;
+}
+
+function setJoao(m){
+    joao = m;
+}
+
+function setMaria(m){
+    maria = m;
+}
